7f9cfa6d8f490987eee10b04c1f7acb8b71ce967
Sheheryar <sheheryar-naveed@hotmail.com>
2018-11-06 20:29:38 -0500

    files upload

---
+++ Assets/Scripts/controller.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Collision2D;
+
+
+public class controller : MonoBehaviour {
+    private bool jump;
+    public float moveSpeed;
+    public float jumpPower;
+    
+    // Use this for initialization
+    void Start () {
+        jump = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.UpArrow) && jump)
+        {
+            rb.AddForce(Vector2.up * jumpPower);
+            jump = false;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            jump = true;
+        }
+    }
+}
---
+++ Assets/Scripts/match_invoke.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class match_invoke : MonoBehaviour {
+
+    public Rigidbody2D match;
+    public Transform player;
+    private bool fire;
+    public GameObject GO;
+    public GameObject go;
+
+    void Start()
+    {
+        fire = true;
+    }
+
+    // Update is called once per frame
+    void Update () {
+        if (Input.GetKey("/") && fire)
+        {
+            //Rigidbody2D matchInstance;
+            go = Instantiate(GO, new Vector2(player.position.x+1f, player.position.y+1f), Quaternion.identity) as GameObject;
+            match = go.GetComponent<Rigidbody2D>();
+            match.AddForce(Vector2.right * 1600);
+            fire = false;
+            Destroy(go, 1.0f);
+         
+        }
+        if (go == null) {
+                fire = true;
+            }
+
+
+    }
+}
