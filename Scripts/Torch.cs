e276e9764fbc64ff9809f218060bf0d6d1ee8b12
lherodes <36320006+lherodes@users.noreply.github.com>
2018-11-26 23:13:57 -0500

    Add files via upload
    This is the assets folder from the scene.

---
+++ Scripts/Torch.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Torch : MonoBehaviour {
+
+    
+    public Light Tool;
+    private int m_nowCount;
+    private bool check;
+    // Use this for initialization
+    void Start () {
+        Tool.enabled = false;
+        check = true;
+        m_nowCount = 0;
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (Input.GetKey(".") && check && m_nowCount <= 3)
+        {
+            check = false;
+            Tool.enabled = true; // for toggling:  !Tool.enabled;
+            m_nowCount++;
+
+            StartCoroutine(LateCall());
+        }
+
+    }
+
+    IEnumerator LateCall()
+    {
+
+        yield return new WaitForSeconds(3);
+        Tool.enabled = false;
+        check = true;
+    }
+
+
+}
---
+++ Scripts/controller.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using Collision2D;
+
+
+public class controller : MonoBehaviour {
+    private bool jump;
+    public float moveSpeed;
+    public float jumpPower;
+
+    private Rigidbody2D mrb2d;
+    public bool onLadder;
+    public float climbSpeed;
+    private float climbVelocity;
+    private float gavityStore;
+
+
+    // Use this for initialization
+    void Start () {
+        jump = true;
+        mrb2d = GetComponent<Rigidbody2D>();
+        gavityStore = mrb2d.gravityScale;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKey(KeyCode.RightArrow))
+            transform.Translate(Vector3.right * moveSpeed * Time.deltaTime);
+        if (Input.GetKey(KeyCode.LeftArrow))
+            transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+
+        Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        if (Input.GetKey(KeyCode.UpArrow) && jump)
+        {
+            rb.AddForce(Vector2.up * jumpPower);
+            jump = false;
+        }
+
+        if (onLadder)
+        {
+            mrb2d.gravityScale = 0f;
+            climbVelocity = climbSpeed * Input.GetAxis("Vertical");
+            mrb2d.velocity = new Vector2(mrb2d.velocity.x, climbVelocity);
+        }
+        if (!onLadder)
+        {
+            mrb2d.gravityScale = gavityStore;
+        }
+
+    }
+
+    void OnCollisionEnter2D(Collision2D other)
+    {
+        if (other.gameObject.tag == "ground")
+        {
+            jump = true;
+        }
+    }
+}
